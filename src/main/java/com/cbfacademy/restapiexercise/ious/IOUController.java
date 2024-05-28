ce62e28a9741dd4d55d1406c5f1976b8b9bfd4be
kikedare78@gmail.com <kikedare78@gmail.com>
2024-05-28 21:16:35 +0100

    Test passed

--- src/main/java/com/cbfacademy/restapiexercise/ious/IOUController.java
+++ src/main/java/com/cbfacademy/restapiexercise/ious/IOUController.java
@@ -14,12 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
- //https://spring.io/guides/gs/accessing-data-mysql
+//https://spring.io/guides/gs/accessing-data-mysql
 /* Implement the Controller
 Create an IOUController class with the following members:
 IOUService iouService - in instance of the service interface
@@ -38,59 +35,76 @@ Set the appropriate accessibility modifiers for all members and annotate the cla
 
 public class IOUController {
 
-@Autowired
-    public IOUService iouService; //- in instance of the service interface
+    @Autowired
+    public IOUService iouService; // - in instance of the service interface
 
-    public IOUController(IOUService iouService){ //- constructor that accepts an instance of the service interface
+    public IOUController(IOUService iouService) { // - constructor that accepts an instance of the service interface
         this.iouService = iouService;
     }
-  // @RequestMapping(path = "/{id}", method = RequestMethod.GET, produces = "application json ")
-    
-    //GET	/api/ious	Get all IOUs
-    @GetMapping 
+    // @RequestMapping(path = "/{id}", method = RequestMethod.GET, produces =
+    // "application json ")
+
+    // GET /api/ious Get all IOUs
+    @GetMapping
     public List<IOU> getAllIOUs() {
 
         return iouService.getAllIOUs();
     }
 
-    //GET	/api/ious/{id}	Get an IOU by id - GET = Read - performed after creating(Post) an id - 
+    // GET /api/ious/{id} Get an IOU by id - GET = Read - performed after
+    // creating(Post) an id -
     @GetMapping("/{id}")
-    
-    public IOU getIOU(@PathVariable UUID id) { 
-        return iouService.getIOU(id);
+
+    public ResponseEntity<IOU> getIOU(@PathVariable UUID id) {
+        try {
+            return new ResponseEntity<>(iouService.getIOU(id), HttpStatus.OK);
+        } catch (NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    //POST	/api/ious	Add an IOU - Post = Create - The first thing to do - C in CRUD
-    @PostMapping 
-    
-    public ResponseEntity <IOU> createIOU(@RequestBody IOU iou) {
+    // POST /api/ious Add an IOU - Post = Create - The first thing to do - C in CRUD
+    @PostMapping
+
+    public ResponseEntity<IOU> createIOU(@RequestBody IOU iou) {
         IOU createdIOU = iouService.createIOU(iou);
         System.out.println(createdIOU.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(createdIOU);
     }
-    //PUT	/api/ious/{id}	Replace an IOU by Id - PUT = update/ rplace an id after creating(Post) it.
+
+    // PUT /api/ious/{id} Replace an IOU by Id - PUT = update/ rplace an id after
+    // creating(Post) it.
     @PutMapping("/{id}")
-    
-    public ResponseEntity <IOU> updatedIOU (@PathVariable UUID id, @RequestBody IOU updatedIOU)  { // replacing the content of the old IOU to new one
+
+    public ResponseEntity<IOU> updatedIOU(@PathVariable UUID id, @RequestBody IOU updatedIOU) { // replacing the content
+                                                                                                // of the old IOU to new
+                                                                                                // one
         try {
             IOU updatediou = iouService.updateIOU(id, updatedIOU);
             return ResponseEntity.status(HttpStatus.OK).body(updatediou);
-        }   
-        catch (NoSuchElementException errorNoSuchElementException) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); 
-        }     
+        } catch (NoSuchElementException errorNoSuchElementException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
-        
-    //DELETE	/api/ious/{id}	Delete an IOU by id - Delete = Delete 
+
+    // DELETE /api/ious/{id} Delete an IOU by id - Delete = Delete
     @DeleteMapping("/{id}")
-    
-    public void deleteIOU (@PathVariable UUID id) {
-        iouService.deleteIOU(id);
+
+    public ResponseEntity<Void> deleteIOU(@PathVariable UUID id) {
+        try {
+            iouService.getIOU(id);
+            iouService.deleteIOU(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
-    //https://www.tutorialspoint.com/postman/postman_put_requests.htm
-    //https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/package-summary.html
-    //https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/package-summary.htm
-    //https://www.javatpoint.com/restful-web-services-delete-resource
-    //Set the appropriate accessibility modifiers for all members and annotate the class as a RestController.
-
+// https://www.tutorialspoint.com/postman/postman_put_requests.htm
+// https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/package-summary.html
+// https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/package-summary.htm
+// https://www.javatpoint.com/restful-web-services-delete-resource
+// Set the appropriate accessibility modifiers for all members and annotate the
+// class as a RestController.
